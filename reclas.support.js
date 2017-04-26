f126b68062bcc900ee29a6e672a23c1d06698e05
johnlenon <johnlenonmaghanoy@gmail.com>
2017-04-26 21:37:21 +0800

    Updated build.

--- reclas.support.js
+++ reclas.support.js
@@ -34,6 +34,9 @@
               			"file": "reclas.js",
               			"module": "reclas",
               			"author": "Richeve S. Bebedor",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+              			],
               			"eMail": "richeve.bebedor@gmail.com",
               			"repository": "https://github.com/volkovasystems/reclas.git",
               			"test": "reclas-test.js",
