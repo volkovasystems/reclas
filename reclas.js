4219c878b4f77a5fd50ab2dd82c04941f2e87301
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-20 13:44:49 +0800

    Updated client test.

--- reclas.js
+++ reclas.js
@@ -34,10 +34,11 @@
 			"file": "reclas.js",
 			"module": "reclas",
 			"author": "Richeve S. Bebedor",
-			"contributors": [
-				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
-			],
 			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
 			"repository": "https://github.com/volkovasystems/reclas.git",
 			"test": "reclas-test.js",
 			"global": true
